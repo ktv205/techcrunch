07f23571096952c6a4e4c54fa470555560fcefd0
krishna teja velagapudi <ktv205@nyu.edu>
2015-05-02 21:00:06 -0400

    test commit from android studio

--- app/src/main/java/com/example/krishnateja/buddiesnearby/Fragments/FriendsFragment.java
+++ app/src/main/java/com/example/krishnateja/buddiesnearby/Fragments/FriendsFragment.java
@@ -1,44 +1,33 @@
 package com.example.krishnateja.buddiesnearby.Fragments;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.widget.SwipeRefreshLayout;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
 
 import com.example.krishnateja.buddiesnearby.R;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-
 /**
  * Created by krishnateja on 4/15/2015.
  */
-public class MainFragment extends Fragment {
+public class FriendsFragment extends Fragment {
 
-    private static final String TAG = MainFragment.class.getSimpleName();
+    private static final String TAG = FriendsFragment.class.getSimpleName();
     private View mView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        mView = inflater.inflate(R.layout.fragment_main, container, false);
-
-
+        mView = inflater.inflate(R.layout.fragment_friends, container, false);
         return mView;
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
 
     }
 
---
+++ app/src/main/java/com/example/krishnateja/buddiesnearby/Utils/FriendsFragmentUtils/FriendsFragmentRecyclerAdapter.java
@@ -0,0 +1,7 @@
+package com.example.krishnateja.buddiesnearby.Utils.FriendsFragmentUtils;
+
+/**
+ * Created by krishnateja on 5/2/2015.
+ */
+public class FriendsFragmentRecyclerAdapter {
+}
